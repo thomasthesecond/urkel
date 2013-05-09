66bb2c5ff823d42e90f1e6eca924891ca8f15a9b
Tom Cunningham <tcunningham@paramore.is>
2013-05-08 21:17:42 -0500

    Version 1.2. Added support for container element.

--- jquery.urkel.js
+++ jquery.urkel.js
@@ -7,18 +7,28 @@
  *
  * Project home: http://thomasthesecond.github.com/urkel
  *
- * Version: 1.1
+ * Version: 1.2
  *
  */
 
-(function($) {
+(function($)
+{
 
-	$.fn.urkel = function(options) {
+	var content,
+		element,
+		accordion,
+		hasContainer = null;
+
+	$.fn.urkel = function(options)
+	{
 
 		// Default settings
-		var settings = $.extend({
+		var settings = $.extend(
+		{
 
 			'accordion' : true,
+			'has_container': false,
+			'container_element': null,
 			'title' : 'h3',
 			'title_closed_class' : 'collapsed',
 			'title_open_class' : 'expanded',
@@ -33,58 +43,109 @@
 		// Find out if this is an accordion
 		var accordion = settings['accordion'];
 
+		// Find out if there is a container element {boolean}
+		var hasContainer = settings['has_container'];
+
 		// Set the content element
-		var content = element + ' > ' + settings['content']; // .accordion > div
+		if(hasContainer == false){
+			content = element + ' > ' + settings['content']; // .accordion > div
+		} else {
+			content = element + ' > ' + settings['container_element'] + ' > ' + settings['content']; // .accordion > [element] > div
+		}
 
-		// For each accordion...
-		$(element).each(function(){
+		// var returnContentElement = (accordion == true) ? accordionTrue() : accordionFalse();
 
+		// For each accordion element...
+		$(element).each(function()
+		{
 			// add the closed class to the title element...
 			$(settings['title']).addClass(settings['title_closed_class']); // h3.collapsed
 
 			// and hide the content elements
 			$(content).css('display', 'none'); // .accordion > div
-
 		});
 
 		// Set the click target element (anchor tag)
 		var target = $(element).find(settings['title']).children('a'); // .accordion > h3 a
 
 		// For each target element...
-		target.each(function(){
-
+		target.each(function()
+		{
 			// get the href attribute
 			var href = $(this).attr('href');
 
 			// Create the click event
-			$(this).click(function(){
-
+			$(this).click(function()
+			{
 				// Toggle between closed class and open class on all siblings of the targeted element's parent title element
-				$(this).parent(settings['title']).toggleClass(settings['title_closed_class']).toggleClass(settings['title_open_class']).siblings(settings['title']);
+				$(this)
+					.parent(settings['title'])
+					.toggleClass(settings['title_closed_class'])
+					.toggleClass(settings['title_open_class'])
+					.siblings(settings['title']);
 
 				// If the title element has the open class, remove it and add the closed class
-				if ($(this).parent(settings['title']).siblings(settings['title']).hasClass(settings['title_open_class'])){
-					$(this).parent(settings['title']).siblings(settings['title']).removeClass(settings['title_open_class']).addClass(settings['title_closed_class']);
-				}
-
-				// Show the targeted element content and hide all others
-				function accordionTrue(){
-					$(content + href).slideToggle(settings['speed']).siblings(settings['content']).slideUp(settings['speed']);
-				}
-
-				// Show the targeted element content but do not hide all others
-				function accordionFalse(){
-					$(content + href).slideToggle(settings['speed']);
+				if(hasContainer == false){
+					if (accordion == true && $(this).parent(settings['title']).siblings(settings['title']).hasClass(settings['title_open_class']))
+						$(this)
+							.parent(settings['title'])
+							.siblings(settings['title'])
+							.removeClass(settings['title_open_class'])
+							.addClass(settings['title_closed_class']);
+				} else {
+					if (accordion == true && $(this).parent(settings['title']).parent(settings['container_element']).siblings().children(settings['title']).hasClass(settings['title_open_class']))
+						$(this)
+							.parent(settings['title'])
+							.parent(settings['container_element'])
+							.siblings()
+							.children(settings['title'])
+							.removeClass(settings['title_open_class'])
+							.addClass(settings['title_closed_class']);
 				}
 
 				// Find out if this is an accordion
-				var returnAccordion = (accordion == true) ? accordionTrue() : accordionFalse();
+				var returnAccordion = (accordion == true) ? accordionTrue(content, href) : accordionFalse(content, href);
 
 				return false;
 
 			});
 
 		});
+
+		/**
+		 * @param {string} content
+		 * @param {string} href
+		 * @returns {void}
+		 */
+		function accordionTrue(content, href)
+		{
+			// Check for container element and then show the
+			// targeted element content and hide all others
+			if(hasContainer == false){
+				$(content + href)
+					.slideToggle(settings['speed'])
+					.siblings(settings['content'])
+					.slideUp(settings['speed']);
+			} else {
+				$(content + href)
+					.slideToggle(settings['speed'])
+					.parent(settings['container_element'])
+					.siblings()
+					.children(settings['content'])
+					.slideUp(settings['speed']);
+			}
+		}
+
+		/**
+		 * @param {string} content
+		 * @param {string} href
+		 * @returns {void}
+		 */
+		function accordionFalse(content, href)
+		{
+			// Show the targeted element content but do not hide all others
+			$(content + href).slideToggle(settings['speed']);
+		}
 
 	};
 
